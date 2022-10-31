8e9e3aadac5fdabe5d5f59d5220b6d2181928c9f
Воронцов Николай <vorontsov@galaktika.ru>
2022-10-31 14:47:34 +0300

    Первая загрузка проекта

---
+++ LGalaktika_Data/lgalaktika.data.h
@@ -0,0 +1,12 @@
+#ifndef LGALAKTIKA_DATA_H
+#define LGALAKTIKA_DATA_H
+
+#include "LGalaktika_Data_global.h"
+
+class LGALAKTIKA_DATA_EXPORT LGalaktika_Data
+{
+public:
+    LGalaktika_Data();
+};
+
+#endif // LGALAKTIKA_DATA_H
